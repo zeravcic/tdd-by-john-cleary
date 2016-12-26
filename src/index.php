d6be3267b8916a2d67ebd82a58ec8923b12c5529
Nikola Zeravcic <niks986@gmail.com>
2016-12-27 00:40:01 +0100

    Day 01: Calc Stats
    Your task is to process a sequence of integer numbers
    to determine the following statistics:
    
    o) minimum value
    o) maximum value
    o) number of elements in the sequence
    o) average value
    
    For example: [6, 9, 15, -2, 92, 11]
    
    o) minimum value = -2
    o) maximum value = 92
    o) number of elements in the sequence = 6
    o) average value = 21.833333

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * PHP version 5.6, 7
+ *
+ * @package Zeravcic\TddByJohnCleary\Test\Day01
+ * @author  Nikola Zeravcic <niks986@gmail.com>
+ * @license <http://opensource.org/licenses/gpl-license.php GPL
+ * @link    http://bytenet.rs Byte Net IT Company
+ */
+
+
+error_reporting(E_ALL);
+
+?>
+
+<h1>TTDs By John Cleary</h1>
+
+<p><em>You can set testing code here</em></p>
+
---
+++ tests/day01/CalcStatsTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * PHP version 5.6, 7
+ *
+ * @package Zeravcic\TddByJohnCleary\Test\Day01
+ * @author  Nikola Zeravcic <niks986@gmail.com>
+ * @license <http://opensource.org/licenses/gpl-license.php GPL
+ * @link    http://nikolazeravcic.iz.rs Personal site
+ */
+
+namespace Zeravcic\TddByJohnCleary\Test\Day01;
+
+use Zeravcic\TddByJohnCleary\Day01\CalcStats;
+
+// require_once __DIR__ . "/../../src/day01/CalcStats.php";
+
+
+/**
+ * Class CalcStatsTest
+ *
+ * @package Zeravcic\TddByJohnCleary\Test\Day01
+ * @author  Nikola Zeravcic <niks986@gmail.com>
+ * @license <http://opensource.org/licenses/gpl-license.php GPL
+ * @link    http://nikolazeravcic.iz.rs Personal site
+ * @see     Zeravcic\TddByJohnCleary\Day01\CalcStats::class
+ */
+class CalcStatsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Testing min value
+     *
+     * @return void
+     */
+    public function testMinValue()
+    {
+        $range = new CalcStats(array(17, -17, 0, 7, 777, 77));
+        $this->assertEquals(-17, $range->minValue());
+    }
+
+    /**
+     * Testing max value
+     *
+     * @return void
+     */
+    public function testMaxValue()
+    {
+        $range = new CalcStats(array(17, -17, 0, 7, 777, 77));
+        $this->assertEquals(777, $range->maxValue());
+    }
+
+    /**
+     * Testing count
+     *
+     * @return void
+     */
+    public function testCount()
+    {
+        $range = new CalcStats(array(17, -17, 0, 7, 777, 77));
+        $this->assertEquals(6, $range->count($range));
+    }
+
+    /**
+     * Testing average value
+     *
+     * @return void
+     */
+    public function testAverageValue()
+    {
+        $range = new CalcStats(array(6, 8, 9, 6, 2, 7));
+        $this->assertEquals(6.33, $range->averageValue($range));
+    }
+}
