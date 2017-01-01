ee3884cf9e25efcffeb927bf22115a625590b31d
Nikola Zeravcic <niks986@gmail.com>
2017-01-02 00:20:38 +0100

    Changed package/repository name
    form zeravcic\tdd_by_john-cleary` to zeravcic\tdd-by-john-cleary`

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,13 +4,16 @@
  *
  * PHP version 5.6, 7
  *
- * @package Zeravcic\TddByJohnCleary\Test\Day01
+ * @package Zeravcic\TddByJohnCleary\Tests\Day01
  * @author  Nikola Zeravcic <niks986@gmail.com>
  * @license <http://opensource.org/licenses/gpl-license.php GPL
  * @link    http://nikolazeravcic.iz.rs Personal site
  */
 
 // Needed for isolated tests
-//require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-require 'vendor/autoload.php';
+ini_set('precision', 14);
+ini_set('serialize_precision', 14);
+
+//require 'vendor/autoload.php';
